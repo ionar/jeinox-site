43a02b1a79d78d5112e27145608382b6cbf91476
Ionar Rafael <irm@ionar.com.br>
2018-06-22 14:46:55 -0300

    remove jekyll and add middleman

---
+++ source/javascripts/site.js
@@ -0,0 +1,9 @@
+// This is where it all goes :)
+// A $( document ).ready() block.
+
+//= require jquery
+//= require bootstrap
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
